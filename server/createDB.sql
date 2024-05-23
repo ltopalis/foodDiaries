b1a083a27419e77fa95480f145dfd585894dc024
Lazaros Topalis <83336644+ltopalis@users.noreply.github.com>
2024-05-23 21:42:03 +0300

    modify DB

--- server/createDB.sql
+++ server/createDB.sql
@@ -1,72 +1,198 @@
--- ****** CREATE DATABASE ********
-CREATE DATABASE IF NOT EXISTS foodDiaries 
-    CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: May 23, 2024 at 06:40 PM
+-- Server version: 8.4.0
+-- PHP Version: 8.2.19
 
-USE foodDiaries;
-
--- ****** CREATE TABLES ********
-CREATE TABLE IF NOT EXISTS person (
-    email       VARCHAR(80)                                             PRIMARY KEY NOT NULL,
-    name        VARCHAR(50)                                             NOT NULL,
-    surname     VARCHAR(50)                                             NOT NULL,
-    password    VARCHAR(20)                                             NOT NULL,
-    last_login  DATETIME                                                DEFAULT NULL,
-    signup_date DATETIME                                                NOT NULL,
-    role        ENUM("TRAINER", "DOCTOR", "USER", "ADMIN", "DIETICIAN") NOT NULL
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS expert (
-    expert_email VARCHAR(80) NOT NULL PRIMARY KEY, 
-    accepted_by  VARCHAR(80) DEFAULT NULL,
-    accepted_on  DATETIME    DEFAULT NULL,
-    degree       BLOB        DEFAULT NULL,
-
-    CONSTRAINT exp_fk_per FOREIGN KEY (expert_email)
-        REFERENCES person(email)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT exp_fk_per_admin FOREIGN KEY (accepted_by)
-        REFERENCES person(email)
-        ON UPDATE CASCADE
-) ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- ****** CREATE FUNCTIONS ********
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `foodDiaries`
+--
 
--- ****** CREATE STORED PROCEDURES ********
-DELIMITER $
+-- --------------------------------------------------------
 
-CREATE PROCEDURE checkNewPerson(IN email VARCHAR(80), OUT checkIfExists VARCHAR(80))
-BEGIN
-    DECLARE mail VARCHAR(80);
+--
+-- Table structure for table `address`
+--
 
-    SELECT person.email INTO mail
-    FROM person
-    WHERE person.email = email;
+CREATE TABLE `address` (
+  `address_name` varchar(80) NOT NULL,
+  `address_number` varchar(7) NOT NULL,
+  `zipCode` varchar(7) NOT NULL,
+  `country` varchar(20) NOT NULL,
+  `address_id` int NOT NULL,
+  `friendly_name` varchar(80) NOT NULL,
+  `person_id` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-    IF mail IS NULL
-    THEN
-        SET checkIfExists = 0;
-    ELSE
-        SET checkIfExists = 1;
-    END IF;
-END$
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `chat`
+--
 
-CREATE PROCEDURE addExpert(IN email VARCHAR(80), IN firstName VARCHAR(50), IN lastName VARCHAR(50), IN password VARCHAR(50), IN role  ENUM("TRAINER", "DOCTOR", "DIETICIAN"), IN degree BLOB)
-BEGIN
+CREATE TABLE `chat` (
+  `user_id` varchar(80) NOT NULL,
+  `sender` varchar(80) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-    INSERT INTO person VALUES (email, name, surname, password, NULL, NOW(), role);
-    INSERT INTO expert VALUES (email, NULL, NULL, degree);
+-- --------------------------------------------------------
 
-END$
+--
+-- Table structure for table `expert`
+--
 
+CREATE TABLE `expert` (
+  `expert_email` varchar(80) NOT NULL,
+  `accepted_by` varchar(80) DEFAULT NULL,
+  `accepted_on` datetime DEFAULT NULL,
+  `degree` blob,
+  `salary` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DELIMITER ;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `Orders`
+--
 
--- ****** INSERT DATA ********
-INSERT INTO person VALUES ("george@gmail.com", "Γεώργιος", "Γεωργίου", "123456", "2024-05-06 15:09:45", "2023-01-15 06:00:00", "ADMIN");
-INSERT INTO person VALUES ("dim@gmail.com", "Δημήτρης", "Δημητρίου", "6548225", "2024-05-06 15:09:45", "2023-01-15 06:00:00", "DOCTOR");
+CREATE TABLE `Orders` (
+  `order_id` int NOT NULL,
+  `date_of_order` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `price` double NOT NULL,
+  `address_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO expert VALUES ("dim@gmail.com", "george@gmail.com", "2023-01-15 06:00:00", NULL);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_products`
+--
+
+CREATE TABLE `order_products` (
+  `order_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `amount` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person`
+--
+
+CREATE TABLE `person` (
+  `email` varchar(80) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lastname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `signup_date` datetime NOT NULL,
+  `role` enum('TRAINER','DOCTOR','USER','ADMIN','DIETICIAN') NOT NULL,
+  `phone_number` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `description` text NOT NULL,
+  `value` double NOT NULL,
+  `added_by` varchar(80) NOT NULL,
+  `image` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_email` varchar(80) NOT NULL,
+  `nutritioan_goal` enum('LOSS','MAINTANCE','GAIN','') NOT NULL,
+  `training_goal` enum('LOSS','MAINTANCE','GAIN','') NOT NULL,
+  `trainer` varchar(80) NOT NULL,
+  `doctor` varchar(80) NOT NULL,
+  `nutritionist` varchar(80) NOT NULL,
+  `weight` float NOT NULL,
+  `height` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallet`
+--
+
+CREATE TABLE `wallet` (
+  `person_id` varchar(80) NOT NULL,
+  `id` int NOT NULL,
+  `value` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`address_id`),
+  ADD KEY `address_fk_person` (`person_id`);
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`user_id`,`sender`,`timestamp`),
+  ADD KEY `sender_fk_person` (`sender`);
+
+--
+-- Indexes for table `expert`
+--
+ALTER TABLE `expert`
+  ADD PRIMARY KEY (`expert_email`),
+  ADD KEY `exp_fk_per_admin` (`accepted_by`);
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `expert`
+--
+ALTER TABLE `expert`
+  ADD CONSTRAINT `exp_fk_per` FOREIGN KEY (`expert_email`) REFERENCES `person` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `exp_fk_per_admin` FOREIGN KEY (`accepted_by`) REFERENCES `person` (`email`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
